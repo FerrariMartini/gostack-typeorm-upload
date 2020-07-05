9eb4d0ea3a0d0ba4ac52c3c3204e26e6899ea843
Ivan Martini <ivan.martini@extremedigital.com.br>
2020-07-04 23:24:48 -0300

    route to delete/remove transactions

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,16 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: number): Promise<void> {
+    const transactionsRepo = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionsRepo.findOne(id);
+
+    if (!transaction) throw new AppError('Transaction not found');
+
+    await transactionsRepo.remove(transaction);
   }
 }
 
